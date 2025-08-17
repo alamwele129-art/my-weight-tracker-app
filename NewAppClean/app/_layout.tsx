7e4927f1a2d9629c566c7d8267bde28cf31f7001
alamwele129-art <alamwele129@gmail.com>
2025-08-17 15:18:42 +0000

    feat: Initialize new EAS project with advanced-weight-tracker slug

--- NewAppClean/app/_layout.tsx
+++ NewAppClean/app/_layout.tsx
@@ -1,8 +1,7 @@
 
 import 'react-native-reanimated';
-import App from '../App.js'; // <-- المسار الصحيح هو '../App'
+import App from '../app.js'; // <-- المسار الصحيح هو '../App'
 
 export default function RootLayout() {
   return <App />;
 }
-```
